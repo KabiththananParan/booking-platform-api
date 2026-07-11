f0ff0038403e065c4f80a9297e2b9f7f27c98601
KabiththananParan <kabiththananparan@gmail.com>
2026-07-11 17:03:29 +0530

    feat: implement users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+
+type CreateUserData = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
+
+  findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  createUser(userData: CreateUserData): Promise<User> {
+    return this.prisma.user.create({
+      data: userData,
+    });
+  }
+}
